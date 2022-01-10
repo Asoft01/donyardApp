5a08e6164c3bc7464a35884d49ad2b842b995843
Adeleke Hammed <lekhad19@gmail.com>
2022-01-10 19:13:43 +0100

    forth commit

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
+
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/donyardapp'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/donyardapp/index.html'));});
-app.listen(process.env.PORT || 8080);
 
+const app = express();
+
+
+// Serve only the static files from the dist directory
+app.use(express.static('./dist/donyardapp'));
+    app.get('/*', function(req, res) {
+        res.sendFile('index.html', {root: '/dist/donyardapp/'}
+    );
+});
+
+app.listen(process.env.PORT || 8080);
 
 /////////////// Second Way of Deploying the Application /////////
 
 // const express = require('express');
 // const path = require('path');
-
 // const app = express();
-
-
-// // Serve only the static files from the dist directory
-// app.use(express.static('./dist/donyard'));
-//     app.get('/*', function(req, res) {
-//         res.sendFile('index.html', {root: '/dist/donyard/'}
-//     );
-// });
-
+// app.use(express.static(__dirname + '/dist/donyardapp'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+ '/dist/donyardapp/index.html'));});
 // app.listen(process.env.PORT || 8080);
 
