6fb08a7126f7945098d57bc6889a3b1173dbbba6
Adeleke Hammed <lekhad19@gmail.com>
2022-01-11 08:57:59 +0100

    minor fix 4

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 
-///////////// Second Way of Deploying the Application /////////
+
 
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/donyardapp'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/donyardapp/index.html'));});
+
+
+// Serve only the static files from the dist directory
+app.use(express.static('./dist/donyardApp'));
+
+    app.get('/*', function(req, res) {
+        res.sendFile('index.html', {root: '/dist/donyardApp/'}
+    );
+});
+
 app.listen(process.env.PORT || 8080);
 
 
 
+///////////// Second Way of Deploying the Application /////////
+
 // const express = require('express');
 // const path = require('path');
-
 // const app = express();
-
-
-// // Serve only the static files from the dist directory
-// app.use(express.static('./dist/donyardapp'));
-//     app.get('/*', function(req, res) {
-//         res.sendFile('index.html', {root: '/dist/donyardapp/'}
-//     );
-// });
-
+// app.use(express.static(__dirname + '/dist/donyardApp'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+ '/dist/donyardApp/index.html'));});
 // app.listen(process.env.PORT || 8080);
