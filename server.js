cc0d05669983c463d488050da72da40c6cf13829
Adeleke Hammed <lekhad19@gmail.com>
2022-01-11 09:03:00 +0100

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static('./dist/donyardApp'));
 
     app.get('/*', function(req, res) {
-        res.sendFile('index.html', {root: '/dist/donyardApp'}
+        res.sendFile('index.html', {root: '/dist/donyardApp/'}
     );
 });
 
